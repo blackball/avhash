ee6b6b4599b165e2e3b2e91f7906f3c2aa69a0a3
blackball <bugway@gmail.com>
2013-07-31 18:24:13 +0800

    a minor format modification

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-###Average Hashing in C
-used for image similarity matching.
+avhash
+====
+
+This would probably be called similarity hashing by other people.
+And I still can not recall why I named this piece of codes "avhash", 
+"average hashing" ? probably because I use the average value to generate 
+the bit vector (feature). It's a kind of like LBP, but I don't think it 
+is the good way now. So what else ?
+
+It's really simple, and an image matching demo was given. There's 
+some further improvement would be pushed in, like make it image size adaptive. 
+
+The hamming distance is really trival to me, how does it handle rotation problem ? 
+Maybe in next push, I will do something related.
+
+
 
--- avhash.c
+++ avhash.c
@@ -1,8 +1,5 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
-#include <opencv/cv.h>
-#include <opencv/highgui.h>
+#include <limits.h> /* CHAR_BIT */
 
 typedef unsigned int uint;
 typedef unsigned char uchar;
@@ -10,136 +7,143 @@ typedef unsigned char uchar;
 #define BITS_PER_UINT (sizeof(uint) * CHAR_BIT)
 
 struct bitvec {
-    uint len;
-    uint code[1];
+        uint len;
+        uint code[1];
 };
 
+/* create bitvec by image width and height */
 struct bitvec*
 bitvec_new(int w, int h) {
-    /* create bitvec by image width and height */
-    struct bitvec *bv = 0;
-    int len = (w * h) / BITS_PER_UINT - 1;
-    if (len < 0) len = 0;
-    bv = (struct bitvec*)calloc(sizeof(*bv) + sizeof(uint) * len, 1);
-    bv->len = len;
-    return bv;
+        struct bitvec *bv = 0;
+        int len = (w * h) / BITS_PER_UINT - 1;
+        if (len < 0) len = 0;
+        bv = (struct bitvec*)calloc(sizeof(*bv) + sizeof(uint) * len, 1);
+        bv->len = len;
+        return bv;
 }
 
 void
 bitvec_free(struct bitvec **bv) {
-    if (bv) {
-	free( *bv );
-	*bv = 0;
-    }
+        if (bv && (*bv)) {
+                free( *bv );
+                *bv = 0;
+        }
 }
 
 /* turn the bit to 1 in position pos */
 #define bitvec_set(bv, pos)						\
-    (bv->code[pos / BITS_PER_UINT] |= (1U << (pos % BITS_PER_UINT)))
+        (bv->code[pos / BITS_PER_UINT] |= (1U << (pos % BITS_PER_UINT)))
 
+/* count 1 in uint */
 static inline uint
 bit_count(uint i) {
-    /* count 1 in uint */
-    uint n = 0;
-    while (i) {
-        n++;
-        i &= (i-1);
-    }
-    return n;
+        uint n = 0;
+        while (i) {
+                n++;
+                i &= (i-1);
+        }
+        return n;
 }
 
+/* calculate the hamming distance of two bitvec */
 uint
 hamming(const struct bitvec *bv0, const struct bitvec *bv1) {
-    /* calculate the hamming distance of two bitvec */
-    uint dist = 0;
+        uint dist = 0;
 
-    for (uint i = 0; i < bv0->len; ++i) {
-        dist += bit_count(bv0->code[i] ^ bv1->code[i]);
-    }
+        for (uint i = 0; i < bv0->len; ++i) {
+                dist += bit_count(bv0->code[i] ^ bv1->code[i]);
+        }
 
-    return dist;
+        return dist;
 }
 
+/* average hashing */
 struct bitvec*
 avhash(const uchar *data, int w, int ws, int h) {
-    /* average hashing */
-    int counter = 0, avg = 0;
-    double davg = .0;
+        int counter = 0, avg = 0;
+        double davg = .0;
 
-    struct bitvec *bv = bitvec_new(w, h);
+        struct bitvec *bv = bitvec_new(w, h);
     
-    for (int i = 0; i < h; ++i)
-	for (int j = 0; j < w; ++j) 
-	    davg += data[i*ws + j];
+        for (int i = 0; i < h; ++i)
+                for (int j = 0; j < w; ++j) 
+                        davg += data[i*ws + j];
 
-    avg = davg / (w * h);
+        avg = davg / (w * h);
     
-    for (int i = 0; i < h; ++i)
-	for (int j = 0; j < w; ++j) {
-	    if (data[i*ws + j] > avg) {
-		bitvec_set(bv, counter);
-	    }
-	    ++ counter;
-	}
+        for (int i = 0; i < h; ++i)
+                for (int j = 0; j < w; ++j) {
+                        if (data[i*ws + j] > avg) {
+                                bitvec_set(bv, counter);
+                        }
+                        ++ counter;
+                }
     
-    return bv;
+        return bv;
 }
 
+#ifdef TEST
+
+#include <stdio.h>
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
 
 static void 
 print_bin(const struct bitvec *bv) {
-    /* print binary representation */
-    for (uint i = 0; i < bv->len; ++i) {
-	uint t = bv->code[i];
-	for (uint j = 1U << (BITS_PER_UINT - 1); j != 0; j >>= 1) {
-	    (t&j) ? putchar('0') : putchar('1');
-	}
-    }
-    putchar('\n');
+        /* print binary representation */
+        for (uint i = 0; i < bv->len; ++i) {
+                uint t = bv->code[i];
+                for (uint j = 1U << (BITS_PER_UINT - 1); j != 0; j >>= 1) {
+                        (t&j) ? putchar('0') : putchar('1');
+                }
+        }
+        putchar('\n');
 }
 
 /* a simple unit test */
 int
 main(int argc, char *argv[]) {
-    const char *name0 = "t.jpg", *name1 = "y.jpg";
-    const CvSize sz = cvSize(16, 32);
-    struct bitvec *bv0, *bv1;
-    IplImage *img, *gray;
-    uint hamming_dist = 0;
+        const char *name0 = "t.jpg", *name1 = "y.jpg";
+        const CvSize sz = cvSize(16, 32);
+        struct bitvec *bv0, *bv1;
+        IplImage *img, *gray;
+        uint hamming_dist = 0;
 
-    if (argc == 3) {
-	name0 = argv[1];
-	name1 = argv[2];
-    }
+        if (argc == 3) {
+                name0 = argv[1];
+                name1 = argv[2];
+        }
     
-    gray = cvCreateImage(sz, 8, 1);
+        gray = cvCreateImage(sz, 8, 1);
 
-    img = cvLoadImage(name0, 0);
-    cvResize(img, gray, CV_INTER_LINEAR);
+        img = cvLoadImage(name0, 0);
+        cvResize(img, gray, CV_INTER_LINEAR);
 
-    bv0 = avhash((const uchar *)(gray->imageData), gray->width, gray->widthStep, gray->height);
+        bv0 = avhash((const uchar *)(gray->imageData), gray->width, gray->widthStep, gray->height);
 
-    cvReleaseImage(&img);
+        cvReleaseImage(&img);
 
-    img = cvLoadImage(name1, 0);
-    cvResize(img, gray, CV_INTER_LINEAR);
+        img = cvLoadImage(name1, 0);
+        cvResize(img, gray, CV_INTER_LINEAR);
 
-    bv1 = avhash((const uchar *)(gray->imageData), gray->width, gray->widthStep, gray->height);
+        bv1 = avhash((const uchar *)(gray->imageData), gray->width, gray->widthStep, gray->height);
 
-    cvReleaseImage(&img);
-    cvReleaseImage(&gray);
+        cvReleaseImage(&img);
+        cvReleaseImage(&gray);
 
 #if 0
-    print_bin(bv0);
-    print_bin(bv1);
+        print_bin(bv0);
+        print_bin(bv1);
 #endif
     
-    hamming_dist = hamming(bv0, bv1);
+        hamming_dist = hamming(bv0, bv1);
 
-    printf("DISTANCE: %d\n", hamming_dist);
+        printf("DISTANCE: %d\n", hamming_dist);
     
-    bitvec_free(&bv0);
-    bitvec_free(&bv1);
+        bitvec_free(&bv0);
+        bitvec_free(&bv1);
 
-    return 0;
+        return 0;
 }
+
+#endif 
